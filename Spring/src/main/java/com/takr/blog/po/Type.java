1820363a4091828e038613d7187ad69e2e37d0fe
TAKR <TAKR011118@outlook.com>
2021-12-01 22:31:19 +0800

    Types
    优化了分类页面，增加了前端检测，还在进行后端检测。

--- Spring/src/main/java/com/takr/blog/po/Type.java
+++ Spring/src/main/java/com/takr/blog/po/Type.java
@@ -2,6 +2,7 @@ package com.takr.blog.po;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class Type {
     @GeneratedValue
     private Long id;
 
+    @NotBlank(message="分类名称不能为空")
     private String name;
 
     @OneToMany(mappedBy = "type")
